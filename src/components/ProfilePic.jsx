1b8637a4987253f0cb5c660c9051912f5a648f8c
EduGese <122921699+EduGese@users.noreply.github.com>
2024-08-27 19:07:22 +0200

    import avatar

--- src/components/ProfilePic.jsx
+++ src/components/ProfilePic.jsx
@@ -1,10 +1,11 @@
 import profilePicture from "../assets/profile-picture.png"
+import avatar from "../assets/avataaars.png"
 import '../css/ProfilePic.css'
 export const ProfilePicture = () => {
     return (
         <div className="frame">
             <img className="front" src={profilePicture} alt="Profile Picture" />
-            <img className="back" src={profilePicture} alt="Profile Picture Back" />
+            <img className="back" src={avatar} alt="Profile Picture Back" />
         </div>
     )
 }
