f69fecacf1e696fa7f463c7f29bc8fff20ae5bdc
EduGese <122921699+EduGese@users.noreply.github.com>
2024-08-30 11:58:11 +0200

    Implement material-ui card

---
+++ src/constants/projects.jsx
@@ -0,0 +1,18 @@
+import { logosLanguages, logosFrameworks, logosDatabase, logosTools } from './logo'
+
+export const projects = [
+    {
+        name:'Task Manager',
+        image: 'src/assets/projects/taskManager.png',
+        description: 'This Ionic 6 Angular Android app empowers you to manage tasks offline. Create, edit, mark tasks complete, and delete them. View pending and completed tasks in separate lists, and track deadlines with a calendar view',
+        technologies: [logosFrameworks[1], logosLanguages[1], logosTools[0], logosDatabase[1]],
+        link: 'https://github.com/EduGese/Task-Manager',
+        demoLink: 'https://github.com/user-attachments/assets/2b04ef06-d5c1-4606-a3b3-526bda4ed2be'
+    },
+    {
+        name:'Task Manager',
+        image: 'src/assets/projects/taskManager.png',
+        description: 'This Ionic 6 Angular Android app empowers you to manage tasks offline. Create, edit, mark tasks complete, and delete them. View pending and completed tasks in separate lists, and track deadlines with a calendar view',
+        technologies: [logosFrameworks[1], logosLanguages[1], logosDatabase[1]]
+    }
+]
