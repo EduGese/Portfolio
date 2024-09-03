06a04cba5fb6ccd6fddc27454515913f6cc47ef1
EduGese <122921699+EduGese@users.noreply.github.com>
2024-09-03 18:12:46 +0200

    Create mainEducation collection

---
+++ src/constants/education.jsx
@@ -0,0 +1,29 @@
+export const mainEducation = [
+    {
+        id: 1,
+        title: 'Bilingual Higher Degree in Web and Mobile Application Development',
+        avatar:'src/assets/education/IESEllago.png',
+        center: 'IES El Lago ',
+        date: '2023',
+        location: 'Madrid, Spain',
+        link:'https://www.educa2.madrid.org/web/centro.ies.ellago.madrid'
+    },
+     {
+        id: 2,
+        title: "Master's Degree in Secondary and Baccalaureate Education Teacher Training",
+        avatar:'src/assets/education/Udima.jpg',
+        center: 'UDIMA',
+        date: '2019',
+        location: 'London, UK',
+        link:'https://www.udima.es/'
+    },
+    {
+        id: 3,
+        title: 'Bachelor of Arts (BA) in History',
+        avatar:'src/assets/education/Uam.png',
+        center: 'UAM',
+        date: '2010',
+        location: 'Madrid, Spain',
+        link:'https://www.uam.es/uam/inicio'
+    },
+]
