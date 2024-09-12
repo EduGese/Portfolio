37c0a7aa4bf133cbc96d6331d315dff1bc8b4a5f
EduGese <122921699+EduGese@users.noreply.github.com>
2024-09-12 17:48:02 +0200

    Styling logoContainer

--- src/components/LogoContainer.jsx
+++ src/components/LogoContainer.jsx
@@ -1,12 +1,43 @@
+import { Box, Typography } from '@mui/material';
+
 export  const LogoContainer = ({href, svg, figCaption}) =>{
 
 
     return(
-        <a href={href}>
-        <figure>
-          {svg}
-          <figcaption>{figCaption}</figcaption>
-        </figure>
-      </a>
+      <a href={href}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          margin: '0',
+          padding: '0',
+          width: {
+            xs: '60px', 
+            sm: '100px', 
+            md: '10vw', 
+            lg: '10vw', 
+          },
+          height: {
+            xs: '60px',
+            sm: '100px',
+            md: '10vw',
+            lg: '10vw',
+          },
+          maxWidth: '10vh',
+          maxHeight: '10vh',
+          position: 'relative',
+          transition: 'transform 0.3s ease-in-out',
+          '&:hover': {
+            transform: 'scale(1.2)',  
+          },
+        }}
+      >
+        {svg}
+        <Typography variant="caption" component="figcaption">
+          {figCaption}
+        </Typography>
+      </Box>
+    </a>
     )
 }
