cdcf4f2bc315822915bfc98288474213e6659af5
EduGese <122921699+EduGese@users.noreply.github.com>
2024-09-13 18:29:52 +0200

    Implement AlertDialogSlide x2

--- src/components/TechnologiesAlertDialogSlide.jsx
+++ src/components/TechnologiesAlertDialogSlide.jsx
@@ -2,10 +2,9 @@ import * as React from 'react';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import Slide from '@mui/material/Slide';
-import {  othersLogos } from '../constants/logo';
+import { othersLogos } from '../constants/logo';
 import { Button, Box } from '@mui/material';
 import { LogoContainer } from '../components/LogoContainer';
 import CancelIcon from '@mui/icons-material/Cancel';
@@ -15,7 +14,7 @@ const Transition = React.forwardRef(function Transition(props, ref) {
   return <Slide direction="left" ref={ref} {...props} />;
 });
 
-export const AlertDialogSlide = () => {
+export const TechnologiesAlertDialogSlide = () => {
   const [open, setOpen] = React.useState(false);
 
   const handleClickOpen = () => {
@@ -29,7 +28,7 @@ export const AlertDialogSlide = () => {
   return (
     <React.Fragment>
       <Button variant="outlined" color="warning" onClick={handleClickOpen}>
-      Also know
+        Also know
       </Button>
       <Dialog
         open={open}
@@ -40,22 +39,22 @@ export const AlertDialogSlide = () => {
       >
         <DialogTitle>{"I also know or have some knowledge of:"}</DialogTitle>
         <DialogContent>
-        <Box
-                sx={{
-                  display: 'flex',
-                  flexDirection: 'row', 
-                 width: 'fit-content',
-                  flexWrap: 'wrap',
-                  overflowX: 'hidden',
-                }}
-              >
-                {othersLogos.map((logo, index) => (
-                  <LogoContainer key={index} href={logo.href} svg={logo.svg} figCaption={logo.figCaption} transition={false}/>
-                ))}
-              </Box>
+          <Box
+            sx={{
+              display: 'flex',
+              flexDirection: 'row',
+              width: 'fit-content',
+              flexWrap: 'wrap',
+              overflowX: 'hidden',
+            }}
+          >
+            {othersLogos.map((logo, index) => (
+              <LogoContainer key={index} href={logo.href} svg={logo.svg} figCaption={logo.figCaption} transition={false} />
+            ))}
+          </Box>
         </DialogContent>
         <DialogActions>
-          <Button onClick={handleClose}><CancelIcon/></Button>
+          <Button onClick={handleClose}><CancelIcon /></Button>
         </DialogActions>
       </Dialog>
     </React.Fragment>
