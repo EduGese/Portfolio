364363e187f5212c14a38f619176b763f78c62fd
EduGese <122921699+EduGese@users.noreply.github.com>
2024-09-21 12:17:29 +0200

    Update viteConfig: - Install vite-plugin-html - Cahnge config to prevent browsers to load cache for url

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,28 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { createHtmlPlugin } from 'vite-plugin-html';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    createHtmlPlugin({
+      inject: {
+        data: {
+          title: 'My portfolio',
+        },
+      },
+      minify: true,
+    }),
+  ],
   build: {
     chunkSizeWarningLimit: 1000,
     rollupOptions: {
       external: ['@mui/icons-material'],
+      output: {
+        entryFileNames: '[name].[hash].js',
+        chunkFileNames: '[name].[hash].js',
+        assetFileNames: '[name].[hash].[ext]',
+      },
     },
-  }
-})
+  },
+});
